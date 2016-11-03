50ce3450880002d19a294c60cf20d0071ef9cb47
kangcy <kangcy@axon.com.cn>
2016-11-03 11:48:42 +0800

    xiugai

--- js/copper2.js
+++ js/copper2.js
@@ -124,7 +124,7 @@ function confirm(callback) {
 
 	if(iscutimging) {
 		var dataURL = $("#readyimg").cropper("getCroppedCanvas");
-		imgurl = dataURL.toDataURL("image/jpeg", 0.6);
+		imgurl = dataURL.toDataURL("image/jpeg", 0.8);
 		Upload(imgurl, callback);
 	} else {
 		if(imgurl.toLowerCase().indexOf("http") > -1) {
@@ -234,5 +234,5 @@ function getBase64Image(img) {
 	canvas.height = height; /*设置新的图片的长度*/
 	var ctx = canvas.getContext("2d");
 	ctx.drawImage(img, 0, 0, width, height); /*绘图*/
-	return canvas.toDataURL("image/jpeg", 0.6);
+	return canvas.toDataURL("image/jpeg", 0.8);
 }
