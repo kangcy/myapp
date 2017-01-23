ba3fb0d5e1e2fe7b3fba72f4978100552b27cb72
kangcy <kangcy@axon.com.cn>
2017-01-23 15:09:56 +0800

    aa

--- js/base.js
+++ js/base.js
@@ -28,7 +28,7 @@ var base = new function() {
 	/**
 	 * 默认图片
 	 **/
-	this.DefaultImg = "../images/logo_default.png";
+	this.DefaultImg = "../images/logo.png";
 
 	/**
 	 * 获取当前用户ID
@@ -512,10 +512,12 @@ var base = new function() {
 		}
 		model.push('</div></div>');
 		//底部统计
-		model.push('<div class="mui-card-footer fl full"><span style="border:1px solid #459df5;color:#459df5;border-radius:5px;padding:2px 5px;margin:0px;" class="f11 fl">' + (item.TypeName == "" ? "其它" : item.TypeName) + '</span><span class="f11">' + item.Views + '次阅 · ' + item.Comments + '评论 · ' + item.Goods + '喜欢 · ' + item.Pays + '打赏</span></div>');
+		model.push('<div class="mui-card-footer fl full"><span class="f12">' + item.Views + '次阅 · ' + item.Comments + '评论 · ' + item.Goods + '喜欢 · ' + item.Pays + '打赏</span><span style="border:1px solid #459df5;color:#459df5;border-radius:5px;padding:2px 5px;margin:0px;" class="f12 fl">' + (item.TypeName == "" ? "其它" : item.TypeName) + '</span></div>');
 		//评论
 		model.push('<div class="mui-card-footer fl full">');
-		console.log(JSON.stringify(item.CommentList))
+		for(var i = 0; i < item.CommentList.length; i++) {
+			model.push('<div class="f12 full fl" style="line-height:1.3rem;"><span class="blue f12">' + item.CommentList[i].UserName + '</span>&nbsp;:<span class="ml5">' + item.CommentList[i].Summary + '</span><div>');
+		}
 		model.push('</div">');
 		if(isdel) {
 			model.push('</div>');
