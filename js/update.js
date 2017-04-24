4d41f588d7ebca6f056e62937140e63eafcdda4b
kangcy <kangcy@axon.com.cn>
2017-04-24 18:30:09 +0800

    aa

--- js/update.js
+++ js/update.js
@@ -18,13 +18,8 @@ function CheckUpdate(callback) {
 				return;
 			}
 			if(data.result) {
-
 				UpdateUrl = data.url;
-				console.log(UpdateUrl);
-				base.RemoveClass(["#myprogressbg", "#myprogress"], ["hide"]);
-				base.AddClass(["#myprogress"], ["bounceInUp"]);
 
-				//document.getElementById("myprogress_desc").innerHtml = "";
 				base.Get("persent").innerHTML = "0%";
 				base.Get("myprogress_desc").innerHTML = "";
 				var remarks = data.remark.split("|");
@@ -32,52 +27,11 @@ function CheckUpdate(callback) {
 				for(var i = 0; i < remarks.length; i++) {
 					remark.push('<p class="full tl mb10">' + remarks[i] + '</p>');
 				}
-				//document.getElementById("myprogress_desc").innerHtml = remark.join("");
 				base.Get("myprogress_desc").innerHTML = remark.join("");
+				base.RemoveClass(["#myprogressbg", "#myprogress"], ["hide"]);
+				base.AddClass(["#myprogress"], ["bounceInUp"]);
 
 				callback(1);
-
-				return;
-
-				var remark = data.remark;
-				remark = remark.replace(/ /g, "\n");
-
-				mui.confirm(remark, '发现最新版本', ['立即更新', '暂不更新'], function(z) {
-					if(z.index == 0) {
-
-						/*base.Get("myprogressbg").classList.remove("hide");
-						base.Get("myprogress").classList.remove("hide");
-						callback(1);
-						base.Get("myprogress").classList.add("bounceIn");*/
-
-						var dtask = plus.downloader.createDownload(data.url, {}, function(d, status) {
-							if(status == 200) {
-								clearInterval(i);
-								base.Get("persent").innerHTML = "100%";
-								plus.nativeUI.toast("正在准备安装环境，请稍后！");
-								setTimeout(function() {
-									var path = d.filename;
-									plus.runtime.install(path); //安装下载的apk文件
-									//清除用户信息
-									var userinfo = {};
-									localStorage.setItem('$userinfo', JSON.stringify(userinfo));
-								}, 1000);
-							} else {
-								mui.toast("更新失败");
-							}
-						});
-						dtask.start();
-						var i = setInterval(function() {
-							var totalSize = dtask.totalSize;
-							var downloadedSize = dtask.downloadedSize;
-							base.Get("proDownFile").setAttribute("value", downloadedSize);
-							base.Get("proDownFile").setAttribute("max", totalSize);
-							if(totalSize > 0) {
-								base.Get("persent").innerHTML = parseInt(100 * downloadedSize / totalSize) + "%";
-							}
-						}, 100);
-					}
-				});
 			}
 		});
 	});
@@ -127,4 +81,6 @@ function Download() {
 //下次再说
 function Next() {
 	base.AddClass(["#myprogress", "#myprogressbg"], ["hide"]);
+	var subindexPage = plus.webview.getWebviewById("subindex");
+	subindexPage.evalJS("PreviewTan2(0)");
 }
