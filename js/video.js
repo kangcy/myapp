2e9899d3195a12c3027a66adcb9ba32ae82a377b
kangcy <kangcy@axon.com.cn>
2017-04-27 17:59:06 +0800

    video

--- js/video.js
+++ js/video.js
@@ -9,20 +9,19 @@ function ShowVideo(domid, vid) {
 }
 
 function AppendVideo(sourceurl) {
-	var html = []; 
-	var width = window.innerWidth * 0.9;
+	var html = [];
 	var height = window.innerHeight * 0.5;
 	//优酷
 	if(sourceurl.indexOf(".") < 0) {
-		return '<div class="media-cont audio-cont" style="width:100%;"><div id="youku_player_' + sourceurl + '" sid="' + sourceurl + '" class="youku_player" style="width:' + width + ';height:' + height + ';"></div></div>';
+		return '<div class="media-cont audio-cont" style="width:100%;"><div id="youku_player_' + sourceurl + '" sid="' + sourceurl + '" class="youku_player" style="width:100%;height:' + height + ';"></div></div>';
 	}
 	//SWF
 	if(sourceurl.toLowerCase().indexOf(".swf") > 0) {
-		return '<div class="media-cont audio-cont" style="width:100%;"><embed src="' + sourceurl + '" allowfullscreen="true" quality="high" width="' + width + '"  height="' + height + '" align="middle" allowscriptaccess="always" type="application/x-shockwave-flash"></embed></div>';
+		return '<div class="media-cont audio-cont" style="width:100%;"><embed src="' + sourceurl + '" allowfullscreen="true" quality="high" width="100%"  height="' + height + '" align="middle" allowscriptaccess="always" type="application/x-shockwave-flash"></embed></div>';
 	}
 	//本地视频
 	if(sourceurl.toLowerCase().indexOf(".") > 0) {
-		return '<video controls="controls" style="width:90%;height:200px;margin:5%;float:left;" preload="Metadata"><source src="' + sourceurl + '" /></video>';
+		return '<video style="width:100%;height:' + height + 'px;" autoplay controls preload="none"><source src="' + sourceurl + '" /></video>';
 	}
 	return "";
 }
